5cf2334de4e9a5a853089ee114ac8ff7d1677041
Robert Peters <r2p2.gw@gmail.com>
2017-09-03 14:47:30 +0200

    Add zoom factor

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ struct Config {
     h: u32,
     center_x: f32,
     center_y: f32,
+    zoom:     f32,
 }
 
 struct Mandelbrot {
@@ -19,7 +20,7 @@ struct Mandelbrot {
 }
 
 impl Mandelbrot {
-    pub fn new(width: u32, center_x: f32, center_y: f32) -> Mandelbrot {
+    pub fn new(width: u32, center_x: f32, center_y: f32, zoom: f32) -> Mandelbrot {
         let height = (width as f32 / 1.75) as u32;
         Mandelbrot {
             conf: Config {
@@ -28,6 +29,7 @@ impl Mandelbrot {
                 h: height,
                 center_x: center_x,
                 center_y: center_y,
+                zoom: zoom,
             },
         }
     }
@@ -52,14 +54,14 @@ impl Mandelbrot {
     }
 
     fn calc_pixel(conf: &Config, x: f32, y: f32) -> u16 {
-        let scaled_x = 3.5 / conf.w as f32;
-        let scaled_y = 2.0 / conf.h as f32;
+        let scaled_x = (1.0 / conf.zoom * 3.5) / conf.w as f32;
+        let scaled_y = (1.0 / conf.zoom * 2.0) / conf.h as f32;
 /*
         let x = i as f32 % conf.w as f32;
         let y = i as f32 / conf.w as f32;
 */
-        let cx = (x as f32 * scaled_x - 1.75) + conf.center_x;
-        let cy = (y as f32 * scaled_y - 1.00) + conf.center_y;
+        let cx = (x as f32 * scaled_x - (1.0 / conf.zoom * 1.75)) + conf.center_x;
+        let cy = (y as f32 * scaled_y - (1.0 / conf.zoom * 1.00)) + conf.center_y;
 
         let c = Complex::new(cx, cy);
         let mut z = c.clone();
@@ -78,9 +80,8 @@ impl Mandelbrot {
     }
 }
 
-
 fn main() {
-    let mb = Mandelbrot::new(4000, -0.75, 0.00);
+    let mb = Mandelbrot::new(4000, -0.75, 0.00, 1.00);
 
     let imgbuf = mb.render();
     let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
